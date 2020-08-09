c086102fab76615844b5e4ed03e8c1efeb70f49e
kdawg <mcleod.karla@gmail.com>
2020-08-09 13:58:20 -0400

    no errors

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
 import React from 'react';
+import logo from './logo.svg';
 import './App.css';
-import Header from './components/Header';
-import About from './components/About';
-import Resume from './components/Resume';
-import Portfolio from './components/Portfolio';
-import Testimonials from './components/Testimonials';
-import Contact from './components/Contact';
-import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <About />
-      <Resume />
-      <Portfolio />
-      <Testimonials />
-      <Contact />
-      <Footer />
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
