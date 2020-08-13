f5653a568474f33e1b1e979aa938463b9e6480e5
kdawg <mcleod.karla@gmail.com>
2020-08-12 20:48:39 -0400

    added header file bones

---
+++ src/components/Header/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import "./style.css";
+
+
+const Header = () => {
+    return (
+        <div>
+            <h1>Karla's React Portfolio</h1>
+            <div className="header_container">
+            </div>
+        </div>
+    )
+}
+
+export default Header
