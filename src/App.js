dfb2837748991df5084c59d6ca846d38098f79e9
kdawg <mcleod.karla@gmail.com>
2020-08-12 21:57:27 -0400

    added header info component

--- src/App.js
+++ src/App.js
@@ -6,9 +6,9 @@ import Header from "./components/Header";
 function App() {
   return (
     <div className="App">
-      <Header />
-
-
+      <div className="App-header">
+        <Header></Header>
+      </div>
     </div>
   );
 }
