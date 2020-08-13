38afa8229e57e039033b68fd709159f6a20bf243
kdawg <mcleod.karla@gmail.com>
2020-08-12 22:43:43 -0400

    added nav

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import React from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.css';
 
+import Nav from './components/Nav'
 import Header from "./components/Header";
+
 
 function App() {
   return (
     <div className="App">
+      <Nav></Nav>
       <div className="App-header">
         <Header></Header>
       </div>
