ff0d00d63686146a5465e8e95a91b7b6c7faa05f
kdawg <mcleod.karla@gmail.com>
2020-08-12 21:19:31 -0400

    created a data file

---
+++ src/mydata.js
@@ -0,0 +1,32 @@
+export default
+    {
+        name: 'Karla McLeod',
+        headerTagline: [
+            'Hi 👋🏼 I’m Karla – a Designer',
+            'of Multiple Mediums 🎨',
+            'from ⛰️ New Hamshire'
+        ],
+        contactEmail: 'mcleod.karla@gmail.com',
+        abouttext: "I also dabble in UI design and Javascript programming. Educated in computer science and web development, I’ve moved into the area of freelancing while working full time on web development for a printing company. I thrive on projects that aim to make a big impact on their industry through innovation and creativity.<br><br>In my free time I enjoy cooking, fishing, hiking and painting. Every year I participate in the Boston Marathon Jimmy Fund Walk. In the past 5 years, my team and I have raised over $20,000 in the fight against cancer. Feel free to join us in the battle by clicking here.<br><br>Being an entrepreneur, I understand the challenges that businesses face when trying to market and promote themselves. I find it a benefit with my experience to put myself in the same position as others wanting incredible results, therefore generating the overall impact customers want. I love working with and getting to know talented people who are in need of creating interesting things. If that’s you, please get in touch!",
+        aboutImage: './karla.jpeg',
+        ShowAboutImage: true,
+        projects: [
+            {
+                id: 1,
+                title: 'Movie Phood',
+                service: 'Front End Role',
+                imageSrc: "",
+                url: 'https://kmcleod81.github.io/project-1/'
+            }
+        ],
+        social: [
+            {
+                name: 'Github',
+                url: 'https://github.com/kmcleod81'
+            },
+            {
+                name: 'LinkedIn',
+                url: 'https://www.linkedin.com/in/karlamcleod/'
+            }
+        ]
+    }
