c819b2d56ee46c6194b2439a1bc9677623ac7272
kdawg <mcleod.karla@gmail.com>
2020-08-15 09:09:32 -0400

    added about section

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import 'bootstrap/dist/css/bootstrap.css';
 
 import Nav from './components/Nav'
 import Header from "./components/Header";
+import About from './components/About';
+
 
 
 function App() {
@@ -12,6 +14,9 @@ function App() {
       <Nav></Nav>
       <div className="App-header">
         <Header></Header>
+      </div>
+      <div className='About'>
+        <About></About>
       </div>
     </div>
   );
