8664917fee867b8c2ddd151b158e281824a139bc
kdawg <mcleod.karla@gmail.com>
2020-08-16 14:22:25 -0400

    updated project font sizes and padding for mobile

--- src/components/ProjectCard/ProjectCard.js
+++ src/components/ProjectCard/ProjectCard.js
@@ -5,7 +5,7 @@ import './project-card.css';
 export const ProjectCard = ({ project }) => {
     return (
         <div className="project-card">
-            <p><b>Name:</b> {project.name}</p><br />
+            <p className="name-bigger"><b>Name:</b> {project.name}</p><br />
             <p><b>Description:</b> {project.description}</p><br />
             <p><b>See it in Action:</b> <a target='_blank' rel="noopener noreferrer" href={project.link}>{project.link}</a> </p>
             <p><b>GitHub Repo:</b> <a target='_blank' rel="noopener noreferrer" href={project.ghlink}>{project.ghlink}</a> </p>
