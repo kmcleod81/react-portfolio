51ba1fd2f900114b31ed4954e3aec8a4de2be7d0
kdawg <mcleod.karla@gmail.com>
2020-08-15 20:50:31 -0400

    deleted all code and started from scratch

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
 import React from 'react';
 import './App.css';
-import 'bootstrap/dist/css/bootstrap.css';
-
-import Nav from './components/Nav'
-import Header from "./components/Header";
-import About from './components/About';
-
-
 
 function App() {
   return (
     <div className="App">
-      <Nav></Nav>
-      <div className="App-header">
-        <Header></Header>
-      </div>
-      <div className='About'>
-        <About></About>
-      </div>
+      <h1>Hello React</h1>
     </div>
   );
 }
