5bd64517d066ef95bfd3f0f3eec908e236a37133
kdawg <mcleod.karla@gmail.com>
2020-08-16 14:08:35 -0400

    added all github links and images

--- src/components/ProjectCard/ProjectCard.js
+++ src/components/ProjectCard/ProjectCard.js
@@ -7,8 +7,8 @@ export const ProjectCard = ({ project }) => {
         <div className="project-card">
             <p><b>Name:</b> {project.name}</p><br />
             <p><b>Description:</b> {project.description}</p><br />
-            <p><b>See it in Action:</b> <a target='_blank' href={project.link}>{project.link}</a> </p>
-            <p><b>GitHub Repo:</b> <a target='_blank' href={project.ghlink}>{project.ghlink}</a> </p>
+            <p><b>See it in Action:</b> <a target='_blank' rel="noopener noreferrer" href={project.link}>{project.link}</a> </p>
+            <p><b>GitHub Repo:</b> <a target='_blank' rel="noopener noreferrer" href={project.ghlink}>{project.ghlink}</a> </p>
             <img
                 src={process.env.PUBLIC_URL + project.image}
                 alt={project.name}
--- src/data/Projects.js
+++ src/data/Projects.js
@@ -12,47 +12,50 @@ export const projects = {
             name: "Online/Offline Budget Tracker",
             description: "This budegt app enables a user to add and subtract their expenses online or offline, incase they need to use the app where there's no service.",
             link: "https://arcane-beach-73879.herokuapp.com/",
-            ghlink: "https://employee-directory-km.herokuapp.com/"
+            ghlink: "https://github.com/kmcleod81/online-offline-budget-tracker",
+            image: "/assets/images/offline.png"
         }, {
             name: "Workout Tracker",
             description: "This is a full stack app that keeps track and displays recent workouts the user inputs into the prompts. The user can add various exercises to thier workout and can track specific info for each exercise. Data is stored in a NoSQL data base using MongoDB and Mongoose.",
             link: "https://calm-reef-67409.herokuapp.com/",
-            ghlink: "https://employee-directory-km.herokuapp.com/"
+            ghlink: "https://github.com/kmcleod81/Workout-Tracker",
+            image: "/assets/images/workout.gif"
         }, {
             name: "Materilaize Portfolio",
             description: "A responsive portfolio page created to show design skills, and projects worked on.",
             link: "https://kmcleod81.github.io/portfolio/",
-            ghlink: "https://employee-directory-km.herokuapp.com/"
+            ghlink: "https://github.com/kmcleod81/portfolio",
+            image: "/assets/images/portfolio.png"
         }, {
             name: "Employee Tracker",
             description: "This application is used to add, update and delete departments, roles and employees into a database connected to mysql.",
             link: "https://kmcleod81.github.io/employee-tracker/",
-            ghlink: "https://employee-directory-km.herokuapp.com/"
-        }, {
-            name: "Project 6",
-            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-            link: "http://madhavbahl.tech/react-pokedex/",
-            ghlink: "https://employee-directory-km.herokuapp.com/"
+            ghlink: "https://github.com/kmcleod81/employee-tracker",
+            image: "/assets/images/empTracker.png"
         }, {
             name: "Digital Dude",
             description: "A retro full-stack clone of the original Tamagotchi game from the 80's. Create an account, add some pets & keep them alive as long as you can.",
             link: "http://www.digital-dude.com/",
-            ghlink: "https://employee-directory-km.herokuapp.com/"
+            ghlink: "https://github.com/kmcleod81/tamagotchi",
+            image: "/assets/images/desktop.gif"
         }, {
             name: "Movie Phood",
             description: "An application to supply the user with a random selection of a movie, food item and beverage based on thier selections they enter.",
             link: "https://gheptig.github.io/project-1/",
-            ghlink: "https://employee-directory-km.herokuapp.com/"
+            ghlink: "https://github.com/kmcleod81/project-1",
+            image: "/assets/images/MoviePhoodProject.gif"
         }, {
             name: "Eat Da Burger",
             description: "This Eat Da Burger application is designed to add and devour any burgers the user wants.",
             link: "https://vast-plateau-18519.herokuapp.com/",
-            ghlink: "https://employee-directory-km.herokuapp.com/"
+            ghlink: "https://github.com/kmcleod81/burger",
+            image: "/assets/images/burger.png"
         }, {
             name: "Bootstrap Portfolio",
             description: "A portfolio page created with HTML, Bootstrap, & CSS",
             link: "https://kmcleod81.github.io/bootstrap-portfolio/",
-            ghlink: "https://employee-directory-km.herokuapp.com/"
+            ghlink: "https://github.com/kmcleod81/bootstrap-portfolio",
+            image: "/assets/images/top.png"
         }
     ]
 }
